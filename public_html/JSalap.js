57c1fbff6be7c3b66803f14377418355b57f70bc
Nagy Domonkos <nagydomonkoskristof@gmail.com>
2021-03-16 14:01:51 +0100

    domonkos

--- public_html/JSalap.js
+++ public_html/JSalap.js
@@ -15,9 +15,39 @@ function osszegzes(){
     }
     
 }
+function feladat3(){
+    feltolt();
+    var x = tomb.length;
+    var visszafele = [];
+    for (var i = 0; i < tomb.length; i++) {
+        visszafele += tomb[x-1]+",";
+        x -= 1;
+    }
+    document.getElementById("feladat3").innerHTML += "<br>" +visszafele;
+}
+function feladat6(){
+    feltolt();
+    f6 = false;
+    for (var i = 0; i < tomb.length; i++) {
+        if (tomb[i]%2==0) {
+            if(tomb[i]%3==0){
+                f6 = true;
+            }
+        }
+    }
+}
+function feladat9(){
+    feltolt();
+    for (var i = 0; i < tomb.length; i++) {
+        for (var i = 0; i < max; i++) {
+            
+        }
+    }
+}
 function szerkezet() {
     feltolt();
     osszegzes();
+    feladat3();
     
     document.getElementById("szamok").innerHTML += "<br>" +tomb+ "<br>Az összeg: "+osszeg;
 }
